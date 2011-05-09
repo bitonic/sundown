83b2b6a43a5a9bb2077c3169ae5fd98c5bc8dbb8
Francesco <f@mazzo.li>
2011-05-09 14:56:06 +0200

    Forgot to add one file

---
+++ src/Text/Upskirt/Flag.hs
@@ -0,0 +1,18 @@
+module Text.Upskirt.Flag
+       ( Flag (..)
+       , toCUInt
+       ) where
+
+
+import Foreign.C.Types
+import Data.Bits
+
+class Flag a where
+  flagIndexes :: a -> [(Int, Bool)]
+
+toCUInt :: Flag a => a -> CUInt
+toCUInt flag = foldl (\uint (ix, b) -> shiftL (fromIntegral . fromEnum $ b) ix .|. uint)
+                     0 (flagIndexes flag)
+
+
+

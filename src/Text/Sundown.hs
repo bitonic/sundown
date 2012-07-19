d3f2ca2fe45312cbb0f83931a3bdd8376dcbafe0
Francesco <f@mazzo.li>
2012-07-19 02:07:58 +0100

    put html renderer one level up, added String and Text

--- src/Text/Sundown.hs
+++ src/Text/Sundown.hs
@@ -3,19 +3,18 @@
 Bindings to the github fork of the sundown library - previously known as
 upskirt: <https://github.com/tanoku/sundown>
 
+To actually get output, you have to use one of the @Text.Sundown.Html@ modules.
+
 Example usage:
 
-> import Text.Sundown
 > import Text.Sundown.Renderers.Html
-> import qualified Data.ByteString as BS
-> import qualified Data.ByteString.UTF8 as UTF8
 > import System (getArgs)
 > import Control.Monad (liftM)
 >
 > main :: IO ()
 > main = do
->   input <- liftM (!! 0) getArgs >>= BS.readFile
->   putStrLn $ UTF8.toString $ renderHtml input allExtensions noHtmlModes Nothing
+>   input <- liftM (!! 0) getArgs >>= readFile
+>   putStrLn $ renderHtml input allExtensions noHtmlModes Nothing
 
 -}
 module Text.Sundown
--- tests/sundown.hs
+++ tests/sundown.hs
@@ -1,12 +1,9 @@
 
-import Text.Sundown
-import Text.Sundown.Renderers.Html
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.UTF8 as UTF8
+import Text.Sundown.Html.String
 import Control.Monad (liftM)
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  input <- liftM (!! 0) getArgs >>= BS.readFile
-  putStrLn $ UTF8.toString $ renderHtml input allExtensions noHtmlModes Nothing
+  input <- liftM (!! 0) getArgs >>= readFile
+  putStrLn $ renderHtml input allExtensions noHtmlModes Nothing
